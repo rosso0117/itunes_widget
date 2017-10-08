6616ed85bc8df7cd16c38072908fa34120f67bf6
Tomohiro Yoshida <rollwithit0117@gmail.com>
2017-10-08 12:47:17 +0900

    first

---
+++ src/components/Form.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    Formページ
+    <HeadComp></HeadComp>
+    <component v-bind:is="isComponent"></component>
+    <button @click="buttonAction()">{{ button }}</button>
+  </div>
+</template>
+
+<script>
+import HeadComp from '@/components/modules/HeadComp.vue'
+import TextareaComp from '@/components/modules/TextareaComp.vue'
+import StringComp from '@/components/modules/StringComp.vue'
+import { mapActions, mapGetters } from 'vuex'
+
+export default {
+  name: 'form',
+  methods: mapActions('Form', {
+    'buttonAction': 'buttonAction'
+  }),
+  computed: mapGetters('Form', {
+    'button': 'getButton',
+    'isComponent': 'getComponent'
+  }),
+  components: {
+    HeadComp,
+    TextareaComp,
+    StringComp
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
